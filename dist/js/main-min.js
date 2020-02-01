8d1dd5bb83c696f4a18ede38ece92d8f7d07ed72
Michael Gad <mokai2008@gmail.com>
2020-02-02 00:22:00 +0200

    lesson 12-14

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-$(".toggle-sidebar").on("click",function(){$(".sidebar, .content-area").toggleClass("no-sidebar")});
+$(".toggle-sidebar").on("click",function(){$(".sidebar, .content-area").toggleClass("no-sidebar")}),$(".toggle-submenu").on("click",function(){$(this).toggleClass("fa-angle-right fa-angle-down"),$(this).parent("a").next(".child-links").slideToggle()});
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,13 @@
+// toggle sidebar
+
 $('.toggle-sidebar').on('click', function() {
   $('.sidebar, .content-area').toggleClass('no-sidebar')
+})
+
+
+// toggle submenu
+
+$('.toggle-submenu').on('click', function() {
+  $(this).toggleClass('fa-angle-right fa-angle-down');
+  $(this).parent('a').next('.child-links').slideToggle()
 })
