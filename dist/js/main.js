03df4dbbced9969ef011a6b88b3608b857e7ff23
Michael Gad <mokai2008@gmail.com>
2020-02-04 00:02:35 +0200

    lesson 18 adding full-screen icon

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,3 +11,19 @@ $('.toggle-submenu').on('click', function() {
   $(this).toggleClass('fa-angle-right fa-angle-down');
   $(this).parent('a').next('.child-links').slideToggle()
 })
+
+// toggle fullscreen
+
+$('.toggle-fullscreen').on('click', function() {
+  $(this).toggleClass('full-screen');
+  if($(this).hasClass('full-screen')) {
+    if(document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen()
+    }
+  } else {
+    if(document.documentElement.exitFullscreen) {
+      document.documentElement.exitFullscreen();
+    }
+  }
+});
+
